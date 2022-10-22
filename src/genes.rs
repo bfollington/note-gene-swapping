31f53b7f56a1638e74a6598b17b3d7969c460c43
Ben Follington <5009316+bfollington@users.noreply.github.com>
2022-10-22 14:20:13 +1000

    Port mutation functions
    Thanks copilot

---
+++ src/genes.rs
@@ -0,0 +1,47 @@
+use crate::util;
+use regex::Regex;
+
+type Gene = String;
+type Genome = Vec<Gene>;
+
+pub fn extract_genes(input: &String) -> Vec<Gene> {
+    let seperator = Regex::new(r"[\.\n]+").expect("Invalid regex");
+    seperator
+      .split(input)
+      .filter(|s| s.len() > 0)
+      .map(|s| s.trim().to_string())
+      .collect()
+}
+
+pub fn swap_random_gene(a: &mut Genome, b: &mut Genome) {
+    let index = util::random_index(a.len());
+
+    let temp = a[index].clone();
+    a[index] = b[index].clone();
+    b[index] = temp;
+}
+
+pub fn share_random_gene(a: &mut Genome, b: &mut Genome) {
+    let index = util::random_index(a.len());
+    let temp = a[index].clone();
+    b.insert(index, temp);
+}
+
+pub fn delete_random_gene(g: &mut Genome) {
+    let index = util::random_index(g.len());
+    g.remove(index);
+}
+
+pub fn duplicate_random_gene(g: &mut Genome) {
+    let index = util::random_index(g.len());
+    let temp = g[index].clone();
+    g.insert(index, temp);
+}
+
+pub fn move_gene_location(g: &mut Genome) {
+    let index = util::random_index(g.len());
+    let destination = util::random_index(g.len());
+    let temp = g[index].clone();
+    g.remove(index);
+    g.insert(destination, temp);
+}
